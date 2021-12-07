b1a55db7d171d54365233cb5c6eda922f46d8831
KoketjoSethobja <sethobjakoketjo90@gmail.com>
2021-12-07 17:03:44 +0200

    Portfolio page commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Home from './components/Home';
+import Portfolio from './components/Portfolio';
+import 'animate.css'
 
 function App() {
 	return (
 		<div className="app">
 			<Home />
+			<Portfolio />
 		</div>
 	);
 }
