6b9ad027fc4a0841d62e7f2d6d096504e75f40b1
KoketjoSethobja <sethobjakoketjo90@gmail.com>
2021-12-07 03:16:01 +0200

    home page commit 'hello'

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,10 +13,20 @@ const Home = () => {
             <div className="right-col">
                 <div className="ryt-content">
                     <div className="intro-me">
-                        <h1>Hello</h1>
+                        <h1 className="hello-text">Hello</h1>
+                        <p className="aboutme-text">I am Koketjo Sethobja and I am an aspiring Software Developer. 
+                            I am a final year student at the North-West University and studying towards
+                            my undergraduate degree in Information Technology (BSc IT). 
+                            I have worked part-time as a Studant Assistant at the North-West University. 
+                            I have worked on a few projects (deployed and not deployed) throughout my degree
+                            to further my skills in the field of Software Development. I have gained as much knowledge from
+                            as I can in the field of Software Development and I would like to know more. I have worked with React Js,
+                            ASP.NET, Node Js and Express Js, HTML&CSS, JavaScript, Java, Oracle SQL, and MySQL.
+                        </p>
                     </div>
                     <div className="why-sovtech">
-
+                        <h1 className="sovtech-question">Why I want to be a developer at SovTech</h1>
+                        <p className="sovtech-answer">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi aperiam quis pariatur, inventore voluptatum ipsum odit non sapiente explicabo officiis ad veritatis incidunt temporibus officia nihil rem earum minus atque velit! Incidunt unde aliquid autem eligendi eveniet corrupti. Necessitatibus explicabo odio harum quaerat libero quibusdam omnis facere quam officia dicta.</p>
                     </div>
                 </div>
             </div>
