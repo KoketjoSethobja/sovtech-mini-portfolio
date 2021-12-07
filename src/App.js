6233858125c3219a7ce1d9c60929a4aa3650d3f7
KoketjoSethobja <sethobjakoketjo90@gmail.com>
2021-12-07 18:22:12 +0200

    projects commit

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import Home from './components/Home';
 import Portfolio from './components/Portfolio';
 import 'animate.css'
+import Contact from './components/Contact';
 
 function App() {
 	return (
 		<div className="app">
 			<Home />
 			<Portfolio />
+			<Contact />
 		</div>
 	);
 }
---
+++ src/components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Contact = () => {
+    return (
+        <div className="contact">
+            <div className="contact-wrap">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Contact;
