8fdb598f6c35a4a1d25133a91b72e885e491b398
KoketjoSethobja <sethobjakoketjo90@gmail.com>
2021-12-07 22:55:47 +0200

    contact me commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div className="home">
+        <div className="home" id="home">
             {/* left col, mobile=top row */}
             <div className="left-col">
                 <div className="pic-div">
